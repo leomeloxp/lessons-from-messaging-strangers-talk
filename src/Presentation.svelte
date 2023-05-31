c5dcf7b07f7716c6493401af7acdaef1b189efe3
Leo Melo <pgp@leomeloxp.dev>
2023-05-31 09:14:22 +0100

    chore: wip add previous slides content to app

--- src/Presentation.svelte
+++ src/Presentation.svelte
@@ -1,28 +1,209 @@
 <script>
-    import Title          from './slides/Title.svelte';
-    import Love           from './slides/Love.svelte';
-    import GettingStarted from './slides/GettingStarted.svelte';
-    import AutoAnimate from './slides/examples/AutoAnimate.svelte';
-    import Backgrounds from './slides/examples/Backgrounds.svelte';
-    import Barebones from './slides/examples/Barebones.svelte';
-    import LayoutHelpers from './slides/examples/LayoutHelpers.svelte';
-    import Markdown from './slides/examples/Markdown.svelte';
-    import Math from './slides/examples/Math.svelte';
-    import Media from './slides/examples/Media.svelte';
-    import Transitions from './slides/examples/Transitions.svelte';
-
-    const partner = ['Svelte', 'Reveal.js'];
+  import Notes from './lib/Notes.svelte';
+  import Slide from './lib/Slide.svelte';
+  import Introduction from './slides/Introduction.svelte';
+  import Title from './slides/Title.svelte';
 </script>
 
-<Title/>
-<Love {partner}/>
-<GettingStarted/>
-<AutoAnimate/>
-<Backgrounds/>
-<Barebones/>
-<LayoutHelpers/>
-<Markdown/>
-<Math/>
-<Media/>
-<Transitions/>
+<Title />
 
+<Introduction />
+
+<Slide>
+  <p>The little island that is your comfort zone</p>
+  <Notes>
+    <ul>
+      <li>2. Slide 2:</li>
+      <li>Learning something new forces us to review our existing assumptions and become humble</li>
+      <li>
+        *"Empty your cup so that it may be filled; become devoid to gain totality."* - Bruce Lee
+      </li>
+      <li>
+        By sending messages to strange I didn't know how they would respond, whether they'd respond
+        at all. Some were helpful, others ghosted me or just didn't show any interest on my initial
+        messages
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>The Power of Human Connection</p>
+  <Notes>
+    <ul>
+      <li>3. Slide 3:</li>
+      <li>
+        Despite the language barrier, after some time I started connecting with people, they weren't
+        all strangers any more
+      </li>
+      <li>
+        Highlight the fact that human connection transcends language and cultural differences.
+      </li>
+      <li>
+        The willing attract, unwilling distract (one another) - Os disposes se atraem, os dispostos
+        se atraem, O Teatro Mágico
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>The Importance of Persistence</p>
+  <Notes>
+    <ul>
+      <li>4. Slide 4:</li>
+      <li>Messaging people every day requires persistence and dedication</li>
+      <li>You have to keep the conversations going, find the time to keep the habit up</li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>The Benefits of Language Learning</p>
+  <Notes>
+    <ul>
+      <li>5. Slide 5:</li>
+      <li>
+        Discuss how even though you didn't speak the language at first, you learned a lot through
+        this experience
+      </li>
+      <li>
+        Highlight the benefits of language learning, such as broadening your horizons and opening up
+        new opportunities
+      </li>
+    </ul>
+    <strong>INTERMISSION</strong>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Learning New Things in Technology - Identifying Your Goal</p>
+  <Notes>
+    <ul>
+      <li>6. Slide 6:</li>
+      <li>
+        Draw a parallel between your talk title and the process for learning new things in
+        technology
+      </li>
+      <li>
+        Discuss the importance of identifying your goals when learning new things in technology
+      </li>
+      <li>
+        For Spanish all I wanted was to listen to music and watch series and not have to worry about
+        subtitles
+      </li>
+      <li>For technology it's up to you but perhaps you could try</li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Learning New Things in Technology - Taking Small Steps</p>
+  <Notes>
+    <ul>
+      <li>7. Slide 7:</li>
+      <li>Just like when starting with Spanish, in coding you should start with the basics</li>
+      <li>Learn some syntax</li>
+      <li>Copy a basic tutorial so you start to see what working code looks like</li>
+      <li>
+        In the case of DevOps, create a basic deployment of a server that returns a hello world
+      </li>
+      <li>Try and change parts of it to get a feel for how things work</li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Learning New Things in Technology - Practising Regularly</p>
+  <Notes>
+    <ul>
+      <li>8. Slide 8:</li>
+      <li>Discuss the importance of regular practice when learning new things in technology</li>
+      <li>Keep your exposure to what you're learning high</li>
+      <li>
+        Read blog posts, listen to podcasts, watch video tutorials or read source code, whatever
+        floats your boat
+      </li>
+      <li>
+        Provide tips for incorporating regular practice into your routine, such as setting aside
+        dedicated time each day or week
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Learning New Things in Technology - Embracing Failure</p>
+  <Notes>
+    <ul>
+      <li>9. Slide 9:</li>
+      <li>Discuss the importance of embracing failure as a natural part of the learning process</li>
+      <li>Provide examples of how you can use failure as an opportunity to learn and improve</li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Learning New Things in Technology - Connecting with Others</p>
+  <Notes>
+    <ul>
+      <li>10. Slide 10:</li>
+      <li>
+        Discuss the importance of connecting with others when learning new things in technology
+      </li>
+      <li>
+        Provide examples of ways to connect with others, such as joining a community of learners or
+        attending meet-ups or conferences
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>The Potential for Positive Impact</p>
+  <Notes>
+    <ul>
+      <li>11. Slide 11:</li>
+      <li>Discuss the potential for small actions to have a big impact</li>
+      <li>
+        Highlight how your experience of sending messages to strangers in a foreign language may
+        have brightened someone's day or made a positive impact in their life
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Conclusion</p>
+  <Notes>
+    <ul>
+      <li>12. Slide 12:</li>
+      <li>Summarize the key points from your talk</li>
+      <li>
+        End with a call to action or a final thought that leaves the audience inspired and motivated
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>References</p>
+  <Notes>
+    <ul>
+      <li>13. Slide 13:</li>
+      <li>
+        Provide a list of references or resources that the audience can use to learn more about the
+        topics you discussed
+      </li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Acknowledgements</p>
+  <Notes>
+    <ul>
+      <li>14. Slide 14:</li>
+      <li>Acknowledge anyone who helped you with your research or preparation for the talk</li>
+    </ul>
+  </Notes>
+</Slide>
+<Slide>
+  <p>Q&A</p>
+  <Notes>
+    <ul>
+      <li>15. Slide 15:</li>
+      <li>
+        End with a slide that encourages the audience to ask questions and engage in discussion
+      </li>
+    </ul>
+  </Notes>
+</Slide>
---
+++ src/slides/Introduction.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Notes from '../lib/Notes.svelte';
+  import Slide from '../lib/Slide.svelte';
+</script>
+
+<Slide>
+  <h2>Hello There 👋</h2>
+
+  <Notes>
+    <ul>
+      <li>1. Slide 1:</li>
+      <li>Introduce myself and the topic of the presentation</li>
+      <li>Talk about being bored during the pandemic and wanting to learn something new</li>
+      <li>
+        Mention that from learning Spanish I was able to realise a few things about the process of
+        learning that I'd like to share
+      </li>
+    </ul>
+  </Notes>
+</Slide>

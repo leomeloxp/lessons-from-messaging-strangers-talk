e9a489ca80e53656569048847ca737656ec5bc0a
Leo Melo <pgp@leomeloxp.dev>
2023-06-30 21:44:37 +0100

    fix: update title

--- src/slides/1_Title.svelte
+++ src/slides/1_Title.svelte
@@ -6,8 +6,7 @@
 <Slide>
   <h1 class=" font-bold flex flex-col justify-between">
     <span class="text-5xl"
-      >Lessons learned from messaging strangers on the internet everyday in a language I did not
-      speak
+      >Lessons learned from messaging strangers on the internet in a language I did not speak
     </span>
 
     <span class="text-4xl">By Leo Melo 🦄 (@leomeloxp)</span>
